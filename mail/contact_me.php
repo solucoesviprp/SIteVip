be9de77863121a29c6ee77ad0b8cd75ded981c18
zanettijm <joao@solucoesvip.com.br>
2017-05-12 12:13:57 -0300

    outro teste email
    outro teste email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,5 @@
 <?php
+/*
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
@@ -22,5 +23,66 @@ $email_body = "Você recebeu uma nova mensagem do site.\n\n"."Mais detalhes:\n\n
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
-return true;         
+return true;    
+*/
+
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['phone'])     ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+   echo "No arguments Provided!";
+   return false;
+   }
+
+
+// Inclui o arquivo class.phpmailer.php localizado na pasta class
+require_once("class/class.phpmailer.php");
+ 
+// Inicia a classe PHPMailer
+$mail = new PHPMailer(true);
+ 
+// Define os dados do servidor e tipo de conexão
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+$mail->IsSMTP(); // Define que a mensagem será SMTP
+ 
+try {
+     $mail->Host = 'mail.industriadesite.com.br'; // Endereço do servidor SMTP (Autenticação, utilize o host smtp.seudomínio.com.br)
+     $mail->SMTPAuth   = true;  // Usar autenticação SMTP (obrigatório para smtp.seudomínio.com.br)
+     $mail->Port       = 25; //  Usar 587 porta SMTP
+     $mail->Username = 'joao@solucoesvip.com.br'; // Usuário do servidor SMTP (endereço de email)
+     $mail->Password = 'vip010203'; // Senha do servidor SMTP (senha do email usado)
+ 
+     //Define o remetente
+     // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=    
+     $mail->SetFrom('joao@solucoesvip.com.br', 'João'); //Seu e-mail
+     $mail->AddReplyTo('joao@solucoesvip.com.br', 'João'); //Seu e-mail
+     $mail->Subject = 'Assunto: Email Site';//Assunto do e-mail
+ 
+ 
+     //Define os destinatário(s)
+     //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+     $mail->AddAddress('joao@solucoesvip.com.br', 'TESTE EMAIL SITE');
+ 
+     //Campos abaixo são opcionais 
+     //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+     //$mail->AddCC('destinarario@dominio.com.br', 'Destinatario'); // Copia
+     //$mail->AddBCC('destinatario_oculto@dominio.com.br', 'Destinatario2`'); // Cópia Oculta
+     //$mail->AddAttachment('images/phpmailer.gif');      // Adicionar um anexo
+ 
+ 
+     //Define o corpo do email
+     $mail->MsgHTML('$message'); 
+ 
+     ////Caso queira colocar o conteudo de um arquivo utilize o método abaixo ao invés da mensagem no corpo do e-mail.
+     //$mail->MsgHTML(file_get_contents('arquivo.html'));
+ 
+     $mail->Send();
+     return true;
+ 
+    //caso apresente algum erro é apresentado abaixo com essa exceção.
+    }catch (phpmailerException $e) {
+      return false; //Mensagem de erro costumizada do PHPMailer
+}     
 ?>
